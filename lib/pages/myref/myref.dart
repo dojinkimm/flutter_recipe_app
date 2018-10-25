58215c00d2027322ed501dfc2446a136ae599a3d
Henry <dojinkim119@gmail.com>
2018-10-25 21:26:24 +0900

    page들

---
+++ lib/pages/myref/myref.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyRef extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("내가 가지고 있는 재료")
+      ),
+    );
+  }
+}

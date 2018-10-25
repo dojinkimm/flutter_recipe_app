5031ca801c549080ed0d5a148b90d74346fdee3d
Henry <dojinkim119@gmail.com>
2018-10-25 21:24:49 +0900

    홈 화면

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:cookday/pages/home.dart';
+import 'package:cookday/pages/splashscreen.dart';
 
+var routes = <String, WidgetBuilder>{
+  "/home": (BuildContext context) => Home(),
+};
 void main() {
   runApp(new MaterialApp(
     theme:
       ThemeData(
-          primaryColor: Colors.redAccent, 
-          accentColor: Colors.orangeAccent, 
+          primaryColor: Color(0xFF4B3082),
+          primarySwatch: Colors.purple,
           fontFamily: 'NotoSansCJKkr',
       ),
     debugShowCheckedModeBanner: false,
-    home: Home(),
+    home: SplashScreen(),
+    routes: routes,
   ));
 }
+
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,39 +1,75 @@
 import 'package:flutter/material.dart';
 
+import 'package:cookday/pages/recipe/recipe.dart';
+import 'package:cookday/pages/category/category.dart';
+import 'package:cookday/pages/myref/myref.dart';
+import 'package:cookday/pages/profile/profile.dart';
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  int _currentIndex;
+  List<Color> _colorOfApp = [Colors.black54,Colors.black54,Colors.black54,Colors.black54];
+      //color of navigation item text & ico
+  final Color _colorOfAppBack =
+      Colors.white; //backgroundcolor of navigation bar
+  final double _fontSize = 15.0;
+
+  @override
+    void initState() {
+      super.initState();
+      _currentIndex = 0;
+    }
+
   @override
   Widget build(BuildContext context) {
+    final List<Widget> _children = [
+      new Recipe(),
+      new Category(),
+      new MyRef(),
+      new Profile()
+    ];
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Cook Day"),
-        automaticallyImplyLeading: false
-      ),
-      body: Center(),
+      body: _children[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-        items:[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text("Home")
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_to_home_screen),
-            title: Text("Home")
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_shopping_cart),
-            title: Text("Home")
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            title: Text("Home")
-          ),
-        ]
-      ),
+          type: BottomNavigationBarType.fixed,
+          onTap: (int index) {
+            setState(() {
+              _currentIndex = index; //아이템 누르면 index지정한다
+              _colorOfApp[_currentIndex] = Color(0xFF4B3082); //현재 누른 아이콘 색깔 변경
+              for(int i=0;i<_colorOfApp.length;i++){//나머지 아이콘들 색깔 원위치
+                if(i == _currentIndex)
+                  continue;
+                _colorOfApp[i] = Colors.black54;
+              }
+            });
+          },
+          currentIndex: _currentIndex,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.restaurant_menu, color: _colorOfApp[0]),
+                title: Text("홈",
+                    style: TextStyle(color: _colorOfApp[0], fontSize: _fontSize)),
+                backgroundColor: _colorOfAppBack),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.menu, color: _colorOfApp[1]),
+                title: Text("카테고리",
+                    style: TextStyle(color: _colorOfApp[1], fontSize: _fontSize)),
+                backgroundColor: _colorOfAppBack),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.kitchen, color: _colorOfApp[2]),
+                title: Text("마이냉장고",
+                    style: TextStyle(color: _colorOfApp[2], fontSize: _fontSize)),
+                backgroundColor: _colorOfAppBack),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle, color: _colorOfApp[3]),
+                title: Text("프로필",
+                    style: TextStyle(color: _colorOfApp[3], fontSize: _fontSize)),
+                backgroundColor: _colorOfAppBack),
+          ]),
     );
   }
 }

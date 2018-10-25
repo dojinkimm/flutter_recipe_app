e6141e46b1bef1d25b5e86d11d4917fbd513f39f
Henry <dojinkim119@gmail.com>
2018-10-25 21:26:11 +0900

    splash screen

--- lib/pages/splashscreen.dart
+++ lib/pages/splashscreen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'package:cookday/pages/mynavigator.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -6,13 +8,75 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 2), () => MyNavigator.goToHome(context));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: new Center(
-        child: Image.asset('images/cookdaylogo.png')
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Opacity(
+            opacity: 0.5,
+            child: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('images/cooksplash.jpg'),
+                      fit: BoxFit.cover)),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 50.0,
+                        child: Icon(
+                          Icons.restaurant_menu,
+                          color: Colors.purple,
+                          size: 50.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                        "CookDay",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25.0,
+                            color: Colors.purple),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Text(
+                  "음식을 만들어봐요",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25.0,
+                      color: Colors.white),
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
-  } 
-} 
+  }
+}

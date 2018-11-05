05c2d9a168fe70b82609e9bce451bc53f3a736fe
Henry <dojinkim119@gmail.com>
2018-11-05 13:29:59 +0900

    settings 부분 추가

--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:cookday/pages/profile/setting.dart';
 
 class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Hello")
+    return Scaffold(
+      body: Center(
+        child: IconButton(
+          icon: Icon(Icons.settings),
+          onPressed:(){
+            Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) =>
+                Setting()));
+          }
+        )
+      ),
     );
   }
 }
